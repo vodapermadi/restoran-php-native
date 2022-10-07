0781f4b95ccafa46696e91c993d7794c7cb4dcca
vodapermadi <yapsitusaya111@gmail.com>
2022-10-08 06:19:55 +0700

    update sql

--- pbo_restoran.sql
+++ pbo_restoran.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2022 at 07:24 AM
+-- Generation Time: Oct 08, 2022 at 01:17 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -24,6 +24,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_cart`
+--
+
+CREATE TABLE `tb_cart` (
+  `id_cart` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_menu`
 --
 
@@ -40,7 +54,32 @@ CREATE TABLE `tb_menu` (
 
 INSERT INTO `tb_menu` (`id_menu`, `name`, `price`, `image_menu`) VALUES
 (12, 'nasi kotak', 25000, '63254b8f282e6.jfif'),
-(13, 'nasi padang', 12000, '63254baa22a3f.jfif');
+(13, 'nasi padang', 14000, '63254baa22a3f.jfif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `menu` varchar(255) NOT NULL,
+  `total` int(11) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `timestamp` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_transaksi`
+--
+
+INSERT INTO `tb_transaksi` (`id_transaksi`, `id_user`, `menu`, `total`, `status`, `timestamp`) VALUES
+(6, 5, 'nasi padang', 14000, 0, '2022-10-07'),
+(7, 5, 'nasi kotak', 25000, 0, '2022-10-07'),
+(8, 5, 'nasi padang', 56000, 0, '2022-10-07'),
+(9, 5, 'nasi kotak', 50000, 0, '2022-10-07');
 
 -- --------------------------------------------------------
 
@@ -62,39 +101,88 @@ CREATE TABLE `tb_user` (
 INSERT INTO `tb_user` (`id_user`, `username`, `password`, `level`) VALUES
 (1, 'vodapermadi', '123', 'admin'),
 (2, 'kasir', '123', 'kasir'),
-(3, 'user', '123', 'user');
+(5, 'user', '123', 'user'),
+(7, 'user2', '123', 'user');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `tb_cart`
+--
+ALTER TABLE `tb_cart`
+  ADD PRIMARY KEY (`id_cart`),
+  ADD KEY `id_cart` (`id_cart`),
+  ADD KEY `id_menu` (`id_menu`),
+  ADD KEY `id_user` (`id_user`);
+
+--
 -- Indexes for table `tb_menu`
 --
 ALTER TABLE `tb_menu`
-  ADD PRIMARY KEY (`id_menu`);
+  ADD PRIMARY KEY (`id_menu`),
+  ADD KEY `id_menu` (`id_menu`);
+
+--
+-- Indexes for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_transaksi` (`id_transaksi`);
 
 --
 -- Indexes for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `tb_cart`
+--
+ALTER TABLE `tb_cart`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
 -- AUTO_INCREMENT for table `tb_menu`
 --
 ALTER TABLE `tb_menu`
-  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_cart`
+--
+ALTER TABLE `tb_cart`
+  ADD CONSTRAINT `tb_cart_ibfk_1` FOREIGN KEY (`id_menu`) REFERENCES `tb_menu` (`id_menu`),
+  ADD CONSTRAINT `tb_cart_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`);
+
+--
+-- Constraints for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD CONSTRAINT `tb_transaksi_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_user` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
